f7b94ab28969922547995ccc71a9e755c6e9b22e
yohan <783b8c87@scimetis.net>
2019-12-04 10:59:37 +0100

    Using HTTPS instead of SSH to query Git repo so the SSH key is not needed.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # --force-recreate is used to recreate container when crontab file has changed
 unset VERSION_ELASTICSEARCH
-VERSION_ELASTICSEARCH=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-elasticsearch.git| head -1 | cut -f 1|cut -c -10) \
+VERSION_ELASTICSEARCH=$(git ls-remote https://git.scimetis.net/yohan/docker-elasticsearch.git| head -1 | cut -f 1|cut -c -10) \
  sudo -E bash -c 'docker-compose up -d --force-recreate'
